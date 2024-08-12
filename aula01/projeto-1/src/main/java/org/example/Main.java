7ddeeb19ff4f715cab27aa577f674238d9fdcb9d
Eduardo Santana <ezambomsantana@gmail.com>
2024-08-12 17:39:34 -0300

    add exercicios

--- aula01/projeto-1/src/main/java/org/example/Main.java
+++ aula01/projeto-1/src/main/java/org/example/Main.java
@@ -19,6 +19,14 @@ public class Main {
             System.out.println(row.vitorias);
         }
 
+        // 1 - Quantas vitórias teve o Botafogo no total?
+        // 2 - Quantas vitórias teve o Corinthians em 2015
+        // 3 - Quantas vitórias, derrotas e empates hoouveram no total?
+        // 4 - Qual time teve mais vitórias, palmeiras ou corinthians?
+        // 5 - Adicione na classe PosicaoClube os gols pró e contra
+        // 6 - Adicione na classe CsvReader a leitura dos gols pró e contra, note que os dados estão em uma única célula, separados por um :
+        // 7 - Some o total de gols feitos por todos os times em 2015
+
     }
 
 }
