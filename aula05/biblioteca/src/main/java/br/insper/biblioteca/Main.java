6ce2f93896de5ee54e62d255524620a161dc661b
Eduardo Santana <ezambomsantana@gmail.com>
2024-08-27 17:42:46 -0300

    adiciona aula05

--- aula05/biblioteca/src/main/java/br/insper/biblioteca/Main.java
+++ aula05/biblioteca/src/main/java/br/insper/biblioteca/Main.java
@@ -2,31 +2,70 @@ package br.insper.biblioteca;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Biblioteca biblioteca = new Biblioteca();
-        biblioteca.setNome("Biblioteca Insper");
-        biblioteca.setEndereco("Rua ABC");
+        Scanner scanner = new Scanner(System.in);
+        Integer opcao = 0;
 
-        Usuario usuario = new Usuario();
-        usuario.setNome("eduardo");
-        usuario.setEmail("a@a.com");
-        usuario.setEndereco("Rua xyz");
+        BibliotecaService bibliotecaService = new BibliotecaService();
+        UsuarioService usuarioService = new UsuarioService();
+        while (opcao != 10) {
 
-        usuario.setBiblioteca(biblioteca);
-        biblioteca.adicionaUsuario(usuario);
+            System.out.println("1 - Cadastrar Biblioteca");
+            System.out.println("2 - Listar Biblitecas");
+            System.out.println("3 - Cadastrar Usuário");
+            System.out.println("4 - Listar Usuários");
+            System.out.println("10 - Sair");
 
-        Emprestimo emprestimo = new Emprestimo();
-        emprestimo.setCodigo("EMP1");
-        emprestimo.setStatus("FINALIZADO");
-        emprestimo.setDataDevolucao(LocalDateTime.now());
+            opcao = scanner.nextInt();
 
-        emprestimo.setUsuario(usuario);
-        usuario.adicionaEmprestimo(emprestimo);
+            if (opcao == 1) {
+                System.out.println("Digite o nome biblioteca:");
+                String nome = scanner.next();
 
+                System.out.println("Digite o endereço da bilioteca");
+                String endereco = scanner.next();
+
+                bibliotecaService.cadastrarBiblioteca(nome, endereco);
+
+            }
+
+            if (opcao == 2) {
+                bibliotecaService.listarBibliotecas();
+            }
+
+            if (opcao == 3) {
+                System.out.println("Digite o nome do usuário:");
+                String nome = scanner.next();
+
+                System.out.println("Digite o email do usuário:");
+                String email = scanner.next();
+
+                System.out.println("Digite o endereco do usuário:");
+                String endereco = scanner.next();
+
+                System.out.println("Digite o nome da biblioteca:");
+                String nomeBiblioteca = scanner.next();
+
+                Biblioteca biblioteca = bibliotecaService.buscarBiblioteca(nomeBiblioteca);
+
+                if (biblioteca != null) {
+                    usuarioService
+                            .cadastrarUsuario(nome, endereco, email, biblioteca);
+                } else {
+                    System.out.println("Erro ao cadastrar usuário.");
+                }
+            }
+
+            if (opcao == 4) {
+                usuarioService.listarUsuarios();
+            }
+
+        }
 
 
     }
