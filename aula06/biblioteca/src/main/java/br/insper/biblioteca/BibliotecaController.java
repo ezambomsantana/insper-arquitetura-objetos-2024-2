9d0be2c1800bb632ffaa07ebda72912cd9126bb8
Eduardo Santana <ezambomsantana@gmail.com>
2024-09-03 17:55:51 -0300

    adiciona projeto biblioteca

---
+++ aula06/biblioteca/src/main/java/br/insper/biblioteca/BibliotecaController.java
@@ -0,0 +1,33 @@
+package br.insper.biblioteca;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class BibliotecaController {
+
+    private BibliotecaService bibliotecaService
+            = new BibliotecaService();
+
+    @PostMapping("/biblioteca")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String postBiblioteca(@RequestBody Biblioteca biblioteca) {
+        bibliotecaService.cadastrarBiblioteca(biblioteca);
+        return "Biblioteca cadastrada com sucesso";
+    }
+
+    @GetMapping("/biblioteca")
+    public ArrayList<Biblioteca> listaBibliotecas(
+            @RequestParam(required = false) String nome) {
+        return bibliotecaService.listarBibliotecas(nome);
+    }
+
+    @DeleteMapping("/biblioteca/{nome}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluirBiblioteca(@PathVariable String nome) {
+        bibliotecaService.excluirBiblioteca(nome);
+    }
+
+}
