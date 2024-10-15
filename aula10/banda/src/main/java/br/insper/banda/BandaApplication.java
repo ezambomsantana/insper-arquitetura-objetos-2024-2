4821db4ecdb5b0b7bd849e00b9a6ee72890e9676
Eduardo Santana <ezambomsantana@gmail.com>
2024-10-15 16:50:56 -0300

    fix cors

--- aula10/banda/src/main/java/br/insper/banda/BandaApplication.java
+++ aula10/banda/src/main/java/br/insper/banda/BandaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BandaApplication {
+public class 	BandaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BandaApplication.class, args);
--- aula10/banda/src/main/java/br/insper/banda/common/WebSecurityConfig.java
+++ aula10/banda/src/main/java/br/insper/banda/common/WebSecurityConfig.java
@@ -1,2 +1,15 @@
-package br.insper.banda.common;public class WebSecurityConfig {
+package br.insper.banda.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+
 }
