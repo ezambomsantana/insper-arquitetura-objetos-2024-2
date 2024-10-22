fc7aebe281436e79417111050cbb0491df08f898
Eduardo Santana <ezambomsantana@gmail.com>
2024-10-22 15:47:16 -0300

    add select

--- aula11/app-1/src/banda/CadastrarBanda.jsx
+++ aula11/app-1/src/banda/CadastrarBanda.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
-import { Button, Grid, TextField } from '@mui/material';
+import { Button, Grid, MenuItem, Select, TextField } from '@mui/material';
 
 function CadastrarBanda() {
 
   const [nome, setNome] = useState();
   const [pais, setPais] = useState();
   const [anoFormacao, setAnoFormacao] = useState();
+  const [idBiblioteca, setIdBiblioteca] = useState();
 
   function click() {
 
     const data = {
       'nome':  nome,
       'pais': pais,
-      'anoFormacao': anoFormacao
+      'anoFormacao': anoFormacao,
+      'biblioteca': {
+        'id': idBiblioteca
+      }
     }
+
+    console.log(data)
 
     fetch('http://localhost:8080/api/v1/banda', {
       method: 'POST',
@@ -50,9 +56,19 @@ function CadastrarBanda() {
       <Grid item xs={6}>
         <TextField
           label='Ano Formação: '
-          valor={anoFormacao}
+          value={anoFormacao}
           onChange={e => setAnoFormacao(e.target.value)}
         />
+      </Grid>
+      <Grid item xs={12}>
+        <Select
+          label='Ano Formação: '
+          value={anoFormacao}
+          onChange={e => setIdBiblioteca(e.target.value)}>
+          <MenuItem value='id1'>Biblioteca 1</MenuItem>
+          <MenuItem value='id1'>Biblioteca 2</MenuItem>
+          <MenuItem value='id1'>Biblioteca 3</MenuItem>
+        </Select>
       </Grid>
       <Grid item xs={12}>
         <Button 
